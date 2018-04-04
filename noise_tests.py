fbff7bbb26df0b9385e913d6bc15cf0dccee4c22
Peter Madigan <pmadigan@berkeley.edu>
2018-04-04 15:41:24 -0700

    Add function to clear buffer to noise_tests

--- noise_tests.py
+++ noise_tests.py
@@ -12,6 +12,16 @@ import time
 import json
 import os
 
+def clear_buffer_quick(controller):
+    controller.run(0.05,'clear buffer (quick)')
+
+def clear_buffer(controller):
+    buffer_clear_attempts = 5
+    clear_buffer_quick(controller)
+    while len(controller.reads[-1]) > 0 and buffer_clear_attempts > 0:
+        clear_buffer_quick(controller)
+        buffer_clear_attempts -= 1
+
 def pulse_channel(controller, chip_idx=0, pulse_channel=0, n_pulses=100,
                   pulse_dac=6, testpulse_dac_max=235, testpulse_dac_min=40):
     '''Simple script to pulse and then listen to a given channel'''
@@ -1420,12 +1430,10 @@ def noise_test_low_threshold(board='pcb-5', chip_idx=0, run_time=1,
     std_dev = {}
     for channel in channel_list:
         print('test channel %d' % channel)
-        #print('clear buffer (quick)')
-        controller.run(0.1,'clear buffer')
+        clear_buffer(controller)
         controller.enable(chip_id=chip.chip_id, io_chain=chip.io_chain,
                           channel_list=[channel])
-        #print('clear buffer (quick)')
-        controller.run(0.1,'clear buffer')
+        clear_buffer_quick(controller)
         controller.run(run_time,'collect data')
         controller.disable(chip_id=chip.chip_id, io_chain=chip.io_chain)
         adc_values[channel] = [packet.dataword for packet in controller.reads[-1]
