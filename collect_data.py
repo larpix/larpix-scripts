388747ae768939a4b2ec42ed5839b990eb7bcc5b
Guest User <Guest@Andreas-MacBook-Pro-2.local>
2018-04-10 12:51:45 -0700

    Update collect_data to use ScriptLogger

--- collect_data.py
+++ collect_data.py
@@ -2,27 +2,55 @@ import time
 import sys
 from larpix.quickstart import *
 from larpix.larpix import enable_logger, flush_logger, SerialPort, disable_logger()
+from helpers.logging import ScriptLogger
 
-enable_logger()
-print 'saving to %s' % SerialPort._logger.filename
-c=qc('pcb-10')
-c.disable()
-load_configurations(c, sys.argv[1])
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', required=True,
+                    help='The configuration to load on the chips (with %d '
+                    'placeholder for chip_id')
+parser.add_argument('--subruns', default=1, required=False, type=int
+                    help='The number of data collection times (default: '
+                    '%(default)s)')
+parser.add_argument('-t','--run_time', default=60, required=False, type=float,
+                    help='The run time for each subrun in sec (default: '
+                    '%(default)s)')
+parser.add_argument('--global_threshold_correction', default=0, required=False,
+                    type=int,
+                    help='Adjustment on global threshold from values in config'
+                    ' files (default: %(default)s)')
+args = parser.parse_args()
 
-if not sys.argv[2] is None:
-    for chip in c.chips:
-        chip.config.global_threshold += int(sys.argv[2])
-        c.write_configuration(chip,32)
+sl = ScriptLogger('data_run')
+log = sl.script_log
+if outdir is None:
+    outdir = sl.script_logdir
 
-for _ in range(30):
-    specifier = time.strftime('%Y_%m_%d_%H_%M_%S')
-    print 'begin collect_data_%s' % specifier
-    c.run(60,'collect_data_%s' % specifier)
-    print 'end collect_data_%s' % specifier
-    print 'storing...'
-    flush_logger()
-    print 'done'
-    c.reads = []
+try:
+    c=qc('pcb-10')
+    c.disable()
+    load_configurations(c, args.config)
 
-print 'end of run %s' % SerialPort._logger.filename
-disable_logger()
+    if not args.global_threshold_correction == 0:
+        for chip in c.chips:
+            chip.config.global_threshold += args.global_threshold_correction
+            c.write_configuration(chip,32)
+
+    for _ in range(args.subruns):
+        specifier = time.strftime('%Y_%m_%d_%H_%M_%S')
+        log.info('begin collect_data_%s' % specifier)
+        c.run(args.run_time,'collect_data_%s' % specifier)
+        log.info('end collect_data_%s' % specifier)
+        log.info('storing...')
+        sl.flush_datalog()
+        log.info('done')
+        c.reads = []
+
+    log.info('end of run %s' % sl.data_logfile)
+except Exception as error:
+    log.exception(error)
+    log.error('run encountered an error')
+    log.info('flushing serial log...')
+    sl.flush_datalog()
+    log.info('done')
+
+    log.info('run closed abnormally: %s' % sl.data_logfile)
