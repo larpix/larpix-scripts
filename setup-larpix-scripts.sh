3555bcd268a225a1a6ac3f6eb9036c7919847ca9
Peter Madigan <pmadigan@berkeley.edu>
2018-04-17 11:02:42 -0700

    Add option to generate default board config from setup-larpix-scripts.sh

--- setup-larpix-scripts.sh
+++ setup-larpix-scripts.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-echo "larpix-scripts home: $(dirname "`pwd`/${BASH_SOURCE[0]}")"
 LARPIX_SCRIPT_DIR="$(dirname "`pwd`/${BASH_SOURCE[0]}")"
+echo "larpix-scripts home: $LARPIX_SCRIPT_DIR"
+# add larpix-scripts to path
+export PYTHONPATH="${PYTHONPATH}:${LARPIX_SCRIPT_DIR}"
+
+if [ ! -z "$1" ]
+# generate a default board layout file (if passed one)
+    then
+    echo "generating default board config: "
+    python_command="
+from helpers.pathnames import make_default_board;
+from time import localtime;
+print make_default_board(localtime(), '$1', force=True)"
+    echo $python_command | python
+fi
 
 echo "creating aliases..."
 alias larpix-check-pedestal="python $LARPIX_SCRIPT_DIR/check_pedestal_width_low_threshold.py"
