aa8c5adcd5c73ca58e6d692a8970a7c34d9dc799
Peter Madigan <pmadigan@berkeley.edu>
2018-05-22 18:33:50 -0700

    Add threshold rate argument to run_configure_chips

--- run_configure_chips.py
+++ run_configure_chips.py
@@ -14,6 +14,9 @@ parser.add_argument('-b', '--board', type=str,
                     help='path to .json file containing chip set info (default: %(default)s)')
 parser.add_argument('-c', '--chip', type=int, nargs='*',
         help='The chip ID(s) to configure', default=[])
+parser.add_argument('-t', '--threshold_rate', type=float, default=5,
+                    help='Threshold rate for configuring chips (optional, units: Hz, default:'
+                    ' %(default)s)')
 args = parser.parse_args()
 
 board_info = json.load(open(args.board,'r'))
@@ -21,8 +24,8 @@ board_info = json.load(open(args.board,'r'))
 for chip in board_info['chip_set']:
     if (not args.chip) or chip[0] in args.chip:
         command = ('python configure_chips.py '
-                   '-o %s -v --chips %d' % (pathnames.default_script_logdir(start_time),
-                                            chip[0]))
+                   '-o %s -v --chips %d --threshold_rate %f' % (\
+                pathnames.default_script_logdir(start_time), chip[0], args.threshold_rate))
         print(command)
         system(command)
                
