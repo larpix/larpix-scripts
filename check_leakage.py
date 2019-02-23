Fix calculation of rms in check_leakage.py and check_pedestal_width_low_threshold.py

check_leakage.py
@@ -12,6 +12,7 @@ Requires a .json file containing chip-ids and daisy chain data formatted like
 '''
 
 from __future__ import print_function
+import numpy as np
 import argparse
 import logging
 from helpers.script_logging import ScriptLogger
@@ -137,8 +138,8 @@ try:
             continue
         chip_mean = sum(board_results[chip_idx]['rate']) /\
             len(board_results[chip_idx]['rate'])
-        chip_rms = sum(abs(rate - chip_mean) for rate in board_results[chip_idx]['rate'])\
-            /len(board_results[chip_idx]['rate'])
+        chip_rms = np.sqrt(sum((rate - chip_mean)**2 for rate in board_results[chip_idx]['rate'])\
+                               /len(board_results[chip_idx]['rate']))
         log.info('%s-%d-c%d mean leakage rate: %.2f Hz, rms: %.2f Hz' % \
                      (board_info, io_chain, chip_id, chip_mean, chip_rms))
         for channel_idx,channel in enumerate(board_results[chip_idx]['channel']):

check_pedestal_width_low_threshold.py
@@ -12,6 +12,7 @@ Requires a .json file containing chip-ids and daisy chain data formatted like
 '''
 
 from __future__ import print_function
+import numpy as np
 import argparse
 import logging
 from helpers.script_logging import ScriptLogger
@@ -128,9 +129,9 @@ try:
             continue
         chip_ped_mean = sum(board_results[chip_idx][0].values()) /\
             len(board_results[chip_idx][0].values())
-        chip_ped_rms = sum(abs(ped - chip_ped_mean)
+        chip_ped_rms = np.sqrt(sum((ped - chip_ped_mean)**2
                            for ped in board_results[chip_idx][0].values()) /\
-                           len(board_results[chip_idx][0])
+                           len(board_results[chip_idx][0]))
         log.info('%s-%d-c%d mean pedestal: %.2f adc, rms: %.2f adc' % \
                      (board_info, io_chain, chip_id, chip_ped_mean, chip_ped_rms))
 